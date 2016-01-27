292251d33f9e780941e50aa056f7bb16c127e784
KC <KC@MacBook-Air.local>
2016-01-26 19:54:00 -0700

    done

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // =======================================================
 
 //1. Log your name to the console (as a string).
-console.log("emran");
+console.log("emran")
 [Log] emran
 //2. Declare a variable called name, log it to the console.
 var name = "emran";
@@ -13,18 +13,18 @@ console.log(name)
 //3. Declare two variables with number values. Add, subtract, multiply and divide them.
 var number1 = 2;
 var number2 = 5;
-number1 + number2
+num1 + num2
 < 7
-number1 - number2
+num1 - num2
 < -3
-number1 * number2
+num1 * num2
 < 10
-number1 / number2
+num1 / num2
 < 0.4
 //4. Declare two variables with string values. Create a third variable that concatenates them.
 >var moo = "Cow";
 >var solar = "Moon";
-> console.log('The ' + moo + ' jumped over the ' + solar + '!');
+> console.log('The ' + moo + ' jumped over the ' + solar + '!')
 [Log] The Cow jumped over the Moon!
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
 var num1 = "5";
@@ -33,20 +33,33 @@ num1 + num2
 < "53"
 num1 - num2
 < 2
-> num1 * num2
+num1 * num2
 < 15
 num1 / num2
 < 1.6666666666666667
 //6. Create a prompt that asks a user to pick a number. Double the answer.
-var number = prompt("Pick a number!?");
+var number = prompt("Pick a Number!?");
 ​7
-console.log(number * 2);
+console.log(number * 2)
 > 14
 //7. Create a custom alert.
 alert("Achtung!");
 //8. Declare three variables called length, width and height. Calculate area and volume.
-
+var length = 2;
+var width = 3;
+var height = 4;
+console.log(length * width * height)
+< 24 (area)
+console.log(length * width)
+< 6 (volume)
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
 // "My name is...I'm....years old, and I live in....."
-
+var name = prompt("Your Name!?")
+var age = prompt("Age!?")
+var city = prompt("And City!?")
+console.log("My Name is " + name + ", Im " + age + " years Old, and I live in " + city + "!")
+< My Name is Java, Im 29 years Old, and I live in Mocha!
 //10. Create an alert that uses the response from a prompt.
+var response = prompt("How about them Broncos!?")
+​alert(response)
+< Awesome!
