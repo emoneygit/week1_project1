1eb83ec771d2f1955870fc7565d31157facda7b3
KC <KC@MacBook-Air.local>
2016-01-26 19:12:53 -0700

    question five

--- main.js
+++ main.js
@@ -4,31 +4,39 @@
 // =======================================================
 
 //1. Log your name to the console (as a string).
-> console.log("emran")
+console.log("emran");
 [Log] emran
 //2. Declare a variable called name, log it to the console.
-> var name = "emran";
-< undefined
-> console.log(name)
+var name = "emran";
+console.log(name)
 [Log] emran
 //3. Declare two variables with number values. Add, subtract, multiply and divide them.
-> var number1 = 2
-> var number2 = 5
-> number1 + number2
+var number1 = 2;
+var number2 = 5;
+number1 + number2
 < 7
-> number1 - number2
+number1 - number2
 < -3
-> number1 * number2
+number1 * number2
 < 10
-> number1 / number2
+number1 / number2
 < 0.4
 //4. Declare two variables with string values. Create a third variable that concatenates them.
->var moo = "Cow"
->var solar = "Moon"
+>var moo = "Cow";
+>var solar = "Moon";
 > console.log('The ' + moo + ' jumped over the ' + solar + '!');
 [Log] The Cow jumped over the Moon!
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
-
+var num1 = "5";
+var num2 = "3";
+num1 + num2
+< "53" = $2
+num1 - num2
+2 = $3
+> num1 * num2
+15 = $4
+num1 / num2
+< 1.6666666666666667 = $1
 //6. Create a prompt that asks a user to pick a number. Double the answer.
 
 //7. Create a custom alert.
